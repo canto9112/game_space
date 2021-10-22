8f15c4d5e346559d0f37eb2d5f20b5b12f6749c1
canto9112 <cantoweb112@gmail.com>
2021-10-22 11:50:08 +0300

    Добавил функцию получения рамки мусора

--- toolkit/frames.py
+++ toolkit/frames.py
@@ -7,3 +7,9 @@ def get_frames(frame_directory):
         with open(f"frames/{frame_directory}/{file}") as f:
             frames.append(f.read())
     return frames
+
+
+def get_garbage_frame(garbage):
+    with open(f'frames/garbage/{garbage}', "r") as garbage_file:
+        frame = garbage_file.read()
+    return frame
