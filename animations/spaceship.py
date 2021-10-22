3c7fcab49d59771da05a8d5c0209c914c5e9cd76
canto9112 <cantoweb112@gmail.com>
2021-10-22 20:56:24 +0300

    Добавил новую физику управления кораблем

--- animations/spaceship.py
+++ animations/spaceship.py
@@ -3,15 +3,18 @@ import asyncio
 from itertools import cycle
 from toolkit.curses_tools import read_controls, draw_frame, get_frame_size
 from toolkit.frames import get_frames
+from toolkit.physics import update_speed
 
 SPACESHIP_FRAMES = get_frames('rocket')
 
 
-async def animate_spaceship(canvas, row, column, max_row, max_column, ):
+async def animate_spaceship(canvas, max_row, max_column, ):
     frame_height, frame_width = get_frame_size(SPACESHIP_FRAMES[0])
 
     row = max_row // 2 - frame_height // 2
     column = max_column // 2 - frame_width // 2
+
+    row_speed, column_speed = 0, 0
 
     for frame in cycle(SPACESHIP_FRAMES):
         draw_frame(canvas, row, column, frame)
@@ -20,13 +23,15 @@ async def animate_spaceship(canvas, row, column, max_row, max_column, ):
 
         draw_frame(canvas, row, column, frame, negative=True)
 
-        row = row + rows_direction
+        row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
+
+        row = row + rows_direction + row_speed
         if row == max_row - frame_height:
             row = row - 1
         elif row == 1:
             row = row + 1
 
-        column = column + columns_direction
+        column = column + columns_direction + column_speed
         if column == max_column - frame_width:
             column = column - 1
         elif column == 1:
