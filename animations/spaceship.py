0c24145259a6dbfc835ac7b1b61d5f7abaf2bd68
canto9112 <cantoweb112@gmail.com>
2021-10-25 12:19:09 +0300

    Добавил к функции анимации корабля аргумент с всеми корутинами

--- animations/spaceship.py
+++ animations/spaceship.py
@@ -4,36 +4,41 @@ from itertools import cycle
 from toolkit.curses_tools import read_controls, draw_frame, get_frame_size
 from toolkit.frames import get_frames
 from toolkit.physics import update_speed
+from animations.fire import fire
 
 SPACESHIP_FRAMES = get_frames('rocket')
 
 
-async def animate_spaceship(canvas, max_row, max_column, ):
+async def animate_spaceship(canvas, max_row, max_column, coroutines):
     frame_height, frame_width = get_frame_size(SPACESHIP_FRAMES[0])
-
     row = max_row // 2 - frame_height // 2
     column = max_column // 2 - frame_width // 2
-
     row_speed, column_speed = 0, 0
-
     for frame in cycle(SPACESHIP_FRAMES):
         draw_frame(canvas, row, column, frame)
         await asyncio.sleep(0)
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
-
         draw_frame(canvas, row, column, frame, negative=True)
-
         row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
-
         row = row + rows_direction + row_speed
         if row == max_row - frame_height:
             row = row - 1
         elif row == 1:
             row = row + 1
-
         column = column + columns_direction + column_speed
         if column == max_column - frame_width:
             column = column - 1
         elif column == 1:
             column = column + 1
+
+        if space_pressed:
+            coroutine_fire = fire(canvas, row, column)
+            coroutines.append(coroutine_fire)
+
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from animations.space_garbage import fill_orbit_garbage
 from toolkit.curses_tools import draw_frame, get_frame_size, read_controls
 from toolkit.frames import get_frames
 from toolkit.physics import update_speed
+from toolkit.obstacles import Obstacle, show_obstacles
+from animations.spaceship import animate_spaceship
+
 
 TIC_TIMEOUT = 0.1
 STARS_AMOUNT = 150
@@ -18,31 +21,31 @@ COROUTINES = []
 GARBAGE_AMOUNT = 10
 
 
-async def animate_spaceship(canvas, max_row, max_column):
-    SPACESHIP_FRAMES = get_frames('rocket')
-    frame_height, frame_width = get_frame_size(SPACESHIP_FRAMES[0])
-    row = max_row // 2 - frame_height // 2
-    column = max_column // 2 - frame_width // 2
-    row_speed, column_speed = 0, 0
-    for frame in cycle(SPACESHIP_FRAMES):
-        draw_frame(canvas, row, column, frame)
-        await asyncio.sleep(0)
-        rows_direction, columns_direction, space_pressed = read_controls(canvas)
-        draw_frame(canvas, row, column, frame, negative=True)
-        row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
-        row = row + rows_direction + row_speed
-        if row == max_row - frame_height:
-            row = row - 1
-        elif row == 1:
-            row = row + 1
-        column = column + columns_direction + column_speed
-        if column == max_column - frame_width:
-            column = column - 1
-        elif column == 1:
-            column = column + 1
-        if space_pressed:
-            coroutine_fire = fire(canvas, row, column)
-            COROUTINES.append(coroutine_fire)
+# async def animate_spaceship(canvas, max_row, max_column):
+#     SPACESHIP_FRAMES = get_frames('rocket')
+#     frame_height, frame_width = get_frame_size(SPACESHIP_FRAMES[0])
+#     row = max_row // 2 - frame_height // 2
+#     column = max_column // 2 - frame_width // 2
+#     row_speed, column_speed = 0, 0
+#     for frame in cycle(SPACESHIP_FRAMES):
+#         draw_frame(canvas, row, column, frame)
+#         await asyncio.sleep(0)
+#         rows_direction, columns_direction, space_pressed = read_controls(canvas)
+#         draw_frame(canvas, row, column, frame, negative=True)
+#         row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
+#         row = row + rows_direction + row_speed
+#         if row == max_row - frame_height:
+#             row = row - 1
+#         elif row == 1:
+#             row = row + 1
+#         column = column + columns_direction + column_speed
+#         if column == max_column - frame_width:
+#             column = column - 1
+#         elif column == 1:
+#             column = column + 1
+#         if space_pressed:
+#             coroutine_fire = fire(canvas, row, column)
+#             COROUTINES.append(coroutine_fire)
 
 
 def draw(canvas):
@@ -63,7 +66,7 @@ def draw(canvas):
     coroutine_fire = fire(canvas, row_center, column_center)
     COROUTINES.append(coroutine_fire)
 
-    coroutine_spaceship = animate_spaceship(canvas, max_row, max_column)
+    coroutine_spaceship = animate_spaceship(canvas, max_row, max_column, COROUTINES)
     COROUTINES.append(coroutine_spaceship)
 
     COROUTINES.extend(fill_orbit_garbage(canvas, max_column) for _ in range(GARBAGE_AMOUNT))
